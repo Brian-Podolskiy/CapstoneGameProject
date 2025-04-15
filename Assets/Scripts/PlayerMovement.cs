a77b95748bebe5e0a8dea3756bac45c1420d41d1
brian-podolskiy <brian_podolskiy@outlook.com>
2025-04-14 20:08:06 -0400

    Added start of attack system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,28 +5,38 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Time timer;
     public Rigidbody2D rb;
     public bool facingRight;
     public Transform groundCheck;
+    public Transform attackPoint;
+    public Transform attackPointLow;
+    public Transform attackPointHigh;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
     public LayerMask groundLayer;
+    // public Animator animator; use for animation
+
 
     private float horizontal;
+    private float startTime;
+    private float elapsedTime;
+    private bool isTiming = false;
     private float speed = 2f;
     private float jumpingPower = 4f;
     private bool faceRight = true;
+    private int lastAttackDone;
 
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
 
-        if (faceRight && horizontal > 0f)
+        if (!faceRight && horizontal > 0f)
         {
             Flip();
         }
@@ -34,6 +44,13 @@ public class PlayerMovement : MonoBehaviour
         {
             Flip();
         }
+    }
+
+    public void StartTimer()
+    {
+        startTime = Time.time;
+        isTiming = true;
+        Debug.Log("timer start");
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -60,10 +77,135 @@ public class PlayerMovement : MonoBehaviour
         Vector3 localScale = transform.localScale;
         localScale.x *= -1f;
         transform.localScale = localScale;
+        Debug.Log("Is facing right: " + faceRight);
     }
 
     public void Move(InputAction.CallbackContext context)
     {
         horizontal = context.ReadValue<Vector2>().x;
+        //Attack(0);
+    }
+
+    public void AttackLow(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Attack(0);
+        }
+        
+    }
+
+    public void AttackMiddle(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Attack(1);
+        }
+        
+    }
+
+    public void AttackHigh(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Attack(2);
+        }
+
+    }
+
+
+    /* Idea for how to do button combos:
+     * player presses attack button
+     * timer starts
+     * if player presses attack within certain number of milliseconds, combo continues
+     * if timer elapses certain point, combo is aborted
+     */
+
+    public void Attack(int type) 
+    {
+        //Debug.Log("Attack");
+        switch (type)
+        {
+            case 0:
+                if (!isTiming)
+                {
+                    StartTimer();
+                }
+
+                else
+                {
+                    elapsedTime = Time.time - startTime;
+                    Debug.Log(elapsedTime);
+                    if (elapsedTime < 5)
+                    {
+                        Debug.Log("continue");
+                        elapsedTime = 0;
+                    }
+                    else
+                    {
+                        Debug.Log("End");
+                        elapsedTime = 0;
+                    }
+                }
+
+                Debug.Log("low");
+                // do an animation
+                Collider2D[] hitEnemyLow = Physics2D.OverlapCircleAll(attackPointLow.position, attackRange, enemyLayers);
+                foreach (Collider2D enemy in hitEnemyLow)
+                {
+                    Debug.Log("hit low");
+                    //do damage here
+                }
+
+                // if animation connects - do damage to enemy
+                lastAttackDone = 0;
+                break;
+            case 1:
+                Debug.Log("mid");
+                // animator.SetTrigger("Attack") do an animation
+                Collider2D[] hitEnemy = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+                foreach (Collider2D enemy in hitEnemy)
+                {
+                    Debug.Log("hit middle");
+                    // do damage here
+                }
+
+                // if animation connects - do damage to enemy
+                lastAttackDone = 1;
+                break;
+            case 2:
+                Debug.Log("high");
+                // do an animation
+                Collider2D[] hitEnemyHigh = Physics2D.OverlapCircleAll(attackPointHigh.position, attackRange, enemyLayers);
+                foreach (Collider2D enemy in hitEnemyHigh)
+                {
+                    Debug.Log("hit high");
+                    // do damage here
+                }
+                lastAttackDone = 2;
+                break;
+
+        }    
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+
+        if (attackPointLow == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPointLow.position, attackRange);
+
+        if (attackPointHigh == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPointHigh.position, attackRange);
     }
 }
